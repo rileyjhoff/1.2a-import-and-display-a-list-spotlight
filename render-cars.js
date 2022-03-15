86201cf1ce88d5885b9bd7f90b4b360a9ff28b29
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-15 14:07:45 -0700

    declared renderCarEl function in render-cars.js

--- render-cars.js
+++ render-cars.js
@@ -1,3 +1,19 @@
-export function renderCarEl() {
+export function renderCarEl(carObject) {
+    const carEl = document.createElement('div');
+    const nameEl = document.createElement('h2');
+    const mpgListEl = document.createElement('ul');
+    const cityEl = document.createElement('li');
+    const highwayEl = document.createElement('li');
+    const msrpEl = document.createElement('p');
+
+    nameEl.textContent = carObject.make + ' ' + carObject.model;
+    mpgListEl.textContent = 'MPG:';
+    cityEl.textContent = `City: ${carObject.city}`;
+    highwayEl.textContent = `Highway: ${carObject.highway}`;
+    msrpEl.textContent = `MSRP: ${carObject.msrp}`;
+
+    mpgListEl.append(cityEl, highwayEl);
+    carEl.append(nameEl, mpgListEl, msrpEl);
+
     return carEl;
 }
