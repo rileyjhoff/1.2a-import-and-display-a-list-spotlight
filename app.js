269a70e63eb12403ceec4e93b87c2dca13a7cf12
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-15 12:46:11 -0700

    wrote loop function in app.js to render and append a div for each state in the states array

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 import { states } from './data-states.js';
 import { renderStateEl } from './render-states.js';
 const stateListEl = document.querySelector('.state-section');
+
+for (let state of states) {
+    const stateEl = renderStateEl(state);
+
+    stateListEl.append(stateEl);
+}
+
