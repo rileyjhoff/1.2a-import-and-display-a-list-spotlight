9b0ab400cb26ec3e14425c630f4b627ab3f07ebf
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-15 11:10:30 -0700

    added an array of objects with car data

---
+++ array-cars.js
@@ -0,0 +1,26 @@
+export const cars = [
+    {
+        make: 'Subaru',
+        model: 'Outback',
+        mpg: '26 city / 33 highway',
+        msrp: '$27,145'
+    },
+    {
+        make: 'Toyota',
+        model: 'RAV4',
+        mpg: '27 city / 35 highway',
+        msrp: '$26,525'
+    },
+    {
+        make: 'Ford',
+        model: 'F-150',
+        mpg: '25 city / 25 highway',
+        msrp: '$29,990'
+    },
+    {
+        make: 'Dodge',
+        model: 'Charger',
+        mpg: '19 city / 30 highway',
+        msrp: '$32,500'
+    }
+];
