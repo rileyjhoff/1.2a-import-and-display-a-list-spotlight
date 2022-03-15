95a40b9d883a26ef9bc43ed843718a75f99400e5
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-15 12:58:01 -0700

    declared renderMarioGolfEl function in render-mario-golf.js

--- render-mario-golf.js
+++ render-mario-golf.js
@@ -1,3 +1,21 @@
-export function renderMarioGolfEl() {
+export function renderMarioGolfEl(characterObject) {
+    const marioGolfEl = document.createElement('div');
+    const nameEl = document.createElement('h2');
+    const specialEl = document.createElement('p');
+    const statListEl = document.createElement('ul');
+    const powerEl = document.createElement('li');
+    const controlEl = document.createElement('li');
+    const spinEl = document.createElement('li');
+
+    nameEl.textContent = characterObject.name;
+    specialEl.textContent = `Special Shot: ${characterObject.special}`;
+    statListEl.textContent = 'Stats:';
+    powerEl.textContent = `Power: ${characterObject.power}`;
+    controlEl.textContent = `Control: ${characterObject.control}`;
+    spinEl.textContent = `Spin: ${characterObject.spin}`;
+
+    statListEl.append(powerEl, controlEl, spinEl);
+    marioGolfEl.append(nameEl, specialEl, statListEl);
+
     return marioGolfEl;
 }
